4b736e38192d8685f7f2df6b64e5e20ccea4e9a2
dr4g0n100 <74046398+dr4g0n100@users.noreply.github.com>
2023-03-30 15:31:02 +0800

    adjustments
    -nav bar tooltip to left
    -createDB fix

--- includes/navbar.php
+++ includes/navbar.php
@@ -16,14 +16,14 @@ $staff_name = preg_replace_callback('/(?:^|\-)\K\w/',
     </li>
       <div class="mr-auto"></div>
       <ul class="navbar-nav">
-        <li class="nav-item " id="navHome" data-toggle="tooltip" data-placement="bottom" title="Homepage">
+        <li class="nav-item " id="navHome" data-toggle="tooltip" data-placement="left" title="Homepage">
           <a class="nav-link" href="indexHomepage.php">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-house-door" viewBox="0 0 16 16">
               <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146ZM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5Z"/>
             </svg>
           </a>
         </li>
-         <li class="nav-item dropdown " id="navCensus" data-toggle="tooltip" data-placement="bottom" title="Census">
+         <li class="nav-item dropdown " id="navCensus" data-toggle="tooltip" data-placement="left" title="Census">
           <a class="nav-link dropdown-toggle" href="indexHomepage.php" id="navbarDropdown" role="button" data-display="static" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-card-checklist" viewBox="0 0 16 16">
               <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/>
@@ -36,7 +36,7 @@ $staff_name = preg_replace_callback('/(?:^|\-)\K\w/',
           </div>
         </li>
         
-        <li class="nav-item admin-nav" id="navUser" data-toggle="tooltip" data-placement="bottom" title="User List">
+        <li class="nav-item admin-nav" id="navUser" data-toggle="tooltip" data-placement="left" title="User List">
           <a class="nav-link admin-nav" href="indexStaff.php?type=checkRecords">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-add" viewBox="0 0 16 16">
               <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0Zm-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z"/>
@@ -44,20 +44,20 @@ $staff_name = preg_replace_callback('/(?:^|\-)\K\w/',
             </svg>
           </a>
         </li>
-        <li class="nav-item dropdown " id="navRecord" data-toggle="tooltip" data-placement="bottom" title="Records">
+        <li class="nav-item dropdown " id="navRecord" data-toggle="tooltip" data-placement="left" title="Records">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-display="static" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-folder-symlink-fill" viewBox="0 0 16 16">
             <path d="M13.81 3H9.828a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 6.172 1H2.5a2 2 0 0 0-2 2l.04.87a1.99 1.99 0 0 0-.342 1.311l.637 7A2 2 0 0 0 2.826 14h10.348a2 2 0 0 0 1.991-1.819l.637-7A2 2 0 0 0 13.81 3zM2.19 3c-.24 0-.47.042-.683.12L1.5 2.98a1 1 0 0 1 1-.98h3.672a1 1 0 0 1 .707.293L7.586 3H2.19zm9.608 5.271-3.182 1.97c-.27.166-.616-.036-.616-.372V9.1s-2.571-.3-4 2.4c.571-4.8 3.143-4.8 4-4.8v-.769c0-.336.346-.538.616-.371l3.182 1.969c.27.166.27.576 0 .742z"/>
             </svg>
           </a>
-          <div class="dropdown-menu dropdown-menu-lg-right" data-toggle="tooltip" data-placement="bottom" aria-labelledby="navbarDropdown">
+          <div class="dropdown-menu dropdown-menu-lg-right" data-toggle="tooltip" data-placement="left" aria-labelledby="navbarDropdown">
             <a class="dropdown-item" href="indexStudent.php?type=checkRecords">Student Records</a>
             <a class="dropdown-item" href="indexCons.php?type=checkRecords">Consultation Records</a>
             <!-- <a class="dropdown-item" href="indexFU.php?type=checkRecords">Follow-up Consultation</a> -->
             <a class="dropdown-item" href="indexMC.php?type=checkMC">Medical Certificate</a>
           </div>
         </li>
-        <li class="nav-item dropdown  admin-nav" id="navArchive" data-toggle="tooltip" data-placement="bottom" title="Archives">
+        <li class="nav-item dropdown  admin-nav" id="navArchive" data-toggle="tooltip" data-placement="left" title="Archives">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-display="static" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-archive" viewBox="0 0 16 16">
               <path d="M0 2a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v7.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 12.5V5a1 1 0 0 1-1-1V2zm2 3v7.5A1.5 1.5 0 0 0 3.5 14h9a1.5 1.5 0 0 0 1.5-1.5V5H2zm13-3H1v2h14V2zM5 7.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
@@ -72,7 +72,7 @@ $staff_name = preg_replace_callback('/(?:^|\-)\K\w/',
             <a class="dropdown-item" href="logs.php?type=checkArchivedLogs">Archived System Logs</a>
           </div>
         </li>
-        <li class="nav-item dropdown admin-nav" id="navMaintenance" data-toggle="tooltip" data-placement="bottom" title="Maintenance">
+        <li class="nav-item dropdown admin-nav" id="navMaintenance" data-toggle="tooltip" data-placement="left" title="Maintenance">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-display="static" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tools" viewBox="0 0 16 16">
               <path d="M1 0 0 1l2.2 3.081a1 1 0 0 0 .815.419h.07a1 1 0 0 1 .708.293l2.675 2.675-2.617 2.654A3.003 3.003 0 0 0 0 13a3 3 0 1 0 5.878-.851l2.654-2.617.968.968-.305.914a1 1 0 0 0 .242 1.023l3.27 3.27a.997.997 0 0 0 1.414 0l1.586-1.586a.997.997 0 0 0 0-1.414l-3.27-3.27a1 1 0 0 0-1.023-.242L10.5 9.5l-.96-.96 2.68-2.643A3.005 3.005 0 0 0 16 3c0-.269-.035-.53-.102-.777l-2.14 2.141L12 4l-.364-1.757L13.777.102a3 3 0 0 0-3.675 3.68L7.462 6.46 4.793 3.793a1 1 0 0 1-.293-.707v-.071a1 1 0 0 0-.419-.814L1 0Zm9.646 10.646a.5.5 0 0 1 .708 0l2.914 2.915a.5.5 0 0 1-.707.707l-2.915-2.914a.5.5 0 0 1 0-.708ZM3 11l.471.242.529.026.287.445.445.287.026.529L5 13l-.242.471-.026.529-.445.287-.287.445-.529.026L3 15l-.471-.242L2 14.732l-.287-.445L1.268 14l-.026-.529L1 13l.242-.471.026-.529.445-.287.287-.445.529-.026L3 11Z"/>
@@ -86,7 +86,7 @@ $staff_name = preg_replace_callback('/(?:^|\-)\K\w/',
             <a class="dropdown-item" href="restore.php">Restore</a>
           </div>
         </li>
-        <li class="nav-item " data-toggle="tooltip" data-placement="bottom" title="User">
+        <li class="nav-item " data-toggle="tooltip" data-placement="left" title="User">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-display="static" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-lines-fill" viewBox="0 0 16 16">
               <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z"/>
