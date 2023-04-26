7e8761d60c6158bd7c0af325347e5c8ec856b0a9
Roe Josept L. Mejia <mejia.roejosept10@gmail.com>
2023-04-26 15:41:19 +0800

    Removed + buttons

--- pages/newConsultation.php
+++ pages/newConsultation.php
@@ -1941,14 +1941,14 @@ $resultVaccine = mysqli_query($connect, $queryVaccine);
                                 <div class="VaccineBrand">
                                     <label for="TxtVaccineBrand" id="VaccineBrand" hidden>Vaccine</label>
                                     <input id="TxtVaccineBrand" list="dlVaccine" name="TxtVaccineBrand" disabled hidden>
-                                    <div class="btn-new-degree" id="NewDegr">
+                                    <!-- <div class="btn-new-degree" id="NewDegr">
                                         <a href="#" role="button" id="new_vaccine_btn" data-toggle="modal" data-target="#DegreeNewModal" hidden>
                                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 14 14" hidden>
                                               <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
                                             </svg>
                                         </a>
 
-                                    </div>
+                                    </div> -->
                                     <datalist id="dlVaccine">
                                         <!-- <option id="AP" value="AztraZeneca Pharmaceuticals"></option>
                                         <option id="BBC" value="Bharat BioTechs Covaxin"></option>
--- pages/newStudent.php
+++ pages/newStudent.php
@@ -4251,14 +4251,14 @@ $resultGrad = mysqli_query($connect, $queryGrad);
 
                                     </datalist>
                                 </div>
-                                <div class="btn-new-degree" id="NewDegr">
+                                <!-- <div class="btn-new-degree" id="NewDegr">
                                     <button type="button" class="btn btn-success" data-toggle="modal" data-target="#DegreeNewModal">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
                                           <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
                                         </svg>
                                     </button>
 
-                                </div>
+                                </div> -->
                             </div>
                             <div class="Two-Info">
                                 <div class="Year">
