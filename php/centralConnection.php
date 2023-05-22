90babff0fb93ef504d14065c8a2d893496f557f9
dr4g0n100 <74046398+dr4g0n100@users.noreply.github.com>
2023-05-22 22:17:51 +0800

    rere

--- php/centralConnection.php
+++ php/centralConnection.php
@@ -9,5 +9,6 @@
   $connectPDO = new PDO("mysql:host=$Server;dbname=$Database", $User, $DBPassword);
   $connection = new mysqli($Server, $User, $DBPassword, $Database);
   
+  mysqli_query($connect, "SET GLOBAL max_allowed_packet=16*1024*1024");
 
 ?>
